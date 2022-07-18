9ed1779a9d1fde67012446db301eb4bc7e698860
bronisMateusz <mateusz.bronis99@gmail.com>
2022-07-18 23:49:31 +0200

    Add visualize theme sceleton

---
+++ web/themes/visualize/js/visualize.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Visualize behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.visualize = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
