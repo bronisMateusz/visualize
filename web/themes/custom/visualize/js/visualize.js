a04081237a04ceea43d8c88a16494e69e34b9689
bronisMateusz <mateusz.bronis99@gmail.com>
2022-08-11 23:31:04 +0200

    Add script to change visible products

--- web/themes/custom/visualize/js/visualize.js
+++ web/themes/custom/visualize/js/visualize.js
@@ -2,71 +2,92 @@
   const productGalleries = document.querySelectorAll(".products-gallery");
 
   if (productGalleries) {
-    const getCurrentProductIndex = (currentProduct) =>
-      Array.prototype.indexOf.call(
-        currentProduct.parentNode.children,
-        currentProduct
-      );
+    const setVisibleProductsImage = (gallery, shiftValue) => {
+      const imageWrappers = gallery.querySelectorAll(".product-image__wrapper");
+      const productsQuantity = imageWrappers.length;
+      const minValue = -2;
+      const maxValue = productsQuantity - 2;
+
+      imageWrappers.forEach((wrapper, wrapperIndex) => {
+        const shiftValueComputed = wrapperIndex - shiftValue;
+
+        Array.from(wrapper.children).forEach((image, imageIndex) => {
+          let rightValue = imageIndex - shiftValueComputed;
+
+          if (rightValue > maxValue) {
+            rightValue = rightValue - productsQuantity;
+          }
+          if (rightValue < minValue) {
+            rightValue = rightValue + productsQuantity;
+          }
+          if (rightValue == minValue) {
+            rightValue = maxValue;
+          }
+
+          rightValue *= 100;
+          image.style.right = rightValue + "%";
+        });
+      });
+    };
+
+    const getCurrentProductIndex = (product) =>
+      Array.prototype.indexOf.call(product.parentNode.children, product);
 
     const getNextProductIndex = (
-      currentProductIndex,
+      currentProduct,
       productsQuantity,
       direction
     ) => {
+      let currentIndex = getCurrentProductIndex(currentProduct);
+      productsQuantity--;
+
       if (direction === "prev") {
-        currentProductIndex !== 0
-          ? currentProductIndex--
-          : (currentProductIndex = productsQuantity);
+        currentIndex !== 0 ? currentIndex-- : (currentIndex = productsQuantity);
       }
-
       if (direction === "next") {
-        currentProductIndex !== productsQuantity
-          ? currentProductIndex++
-          : (currentProductIndex = 0);
+        currentIndex !== productsQuantity ? currentIndex++ : (currentIndex = 0);
       }
 
-      return currentProductIndex;
+      return currentIndex;
     };
 
-    const removeActiveElement = (element) =>
+    const hideProductDetails = (element) =>
       (element.className = element.className.replace("__active", ""));
 
-    const setActiveElement = (element) =>
+    const showProductDetails = (element) =>
       (element.className = element.className + "__active");
 
-    const changeCurrentProduct = (productGallery, direction) => {
-      const productsImage = productGallery.querySelectorAll(
-        ".product-image__wrapper"
-      );
-      const productsDetails = productGallery.querySelectorAll(
+    const changeCurrentProduct = (gallery, direction) => {
+      const productsDetails = gallery.querySelectorAll(
         "[class*='product-details']"
       );
-      const currentProduct = productGallery.querySelector(
-        ".product-details__active"
-      );
-      const currentProductIndex = getCurrentProductIndex(currentProduct);
-      const productsQuantity = productsDetails.length - 1;
-      const nextProductIndex = getNextProductIndex(
-        currentProductIndex,
+      const productsQuantity = productsDetails.length;
+      const currentProduct = gallery.querySelector(".product-details__active");
+      const nextIndex = getNextProductIndex(
+        currentProduct,
         productsQuantity,
         direction
       );
 
-      removeActiveElement(currentProduct);
-      setActiveElement(productsDetails[nextProductIndex]);
-      console.log(productsDetails[nextProductIndex]);
+      hideProductDetails(currentProduct);
+      showProductDetails(productsDetails[nextIndex]);
+
+      const shiftValue = productsQuantity - nextIndex;
+      setVisibleProductsImage(gallery, shiftValue);
     };
 
-    productGalleries.forEach((productGallery) => {
-      const prevButton = productGallery.querySelector(".prev-product");
-      const nextButton = productGallery.querySelector(".next-product");
+    productGalleries.forEach((gallery) => {
+      setVisibleProductsImage(gallery, 0);
+
+      const prevButton = gallery.querySelector(".prev-product");
+      const nextButton = gallery.querySelector(".next-product");
 
       prevButton.addEventListener("click", () => {
-        changeCurrentProduct(productGallery, "prev");
+        changeCurrentProduct(gallery, "prev");
       });
 
       nextButton.addEventListener("click", () => {
-        changeCurrentProduct(productGallery, "next");
+        changeCurrentProduct(gallery, "next");
       });
     });
   }
