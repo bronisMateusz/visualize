b4a5c77732f2a1b8b0c6a063c309a3f613b795bc
bronisMateusz <mateusz.bronis99@gmail.com>
2022-09-06 19:35:55 +0200

    Rebuild vite.js config

--- www/web/themes/custom/visualize/vite.config.js
+++ www/web/themes/custom/visualize/vite.config.js
@@ -1,45 +1,25 @@
+import { defineConfig } from "vite";
 import liveReload from "vite-plugin-live-reload";
+import multiInput from "rollup-plugin-multi-input";
 
-const fs = require("fs");
-const scriptsDir = "./js";
-const scriptsFIles = fs.readdirSync(scriptsDir).map((file) => `${scriptsDir}/${file}`);
-const files = scriptsFIles.concat(["/scss/styles.scss"]);
-
-export default {
-  plugins: [liveReload(__dirname + "/**/*.(php|inc|theme|twig|scss)")],
-  base: "/themes/custom/visualize/dist/",
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: files,
-      // Remove the [hash] since Drupal will take care of that.
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `chunks/[name].[hash].js`,
-        assetFileNames: `[name].[ext]`,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [multiInput(), liveReload(["*.theme", "templates/**/*.twig"])],
+    base:
+      mode === "production"
+        ? "/themes/custom/visualize/dist/"
+        : "/themes/custom/visualize/",
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: ["scss/**/*.scss", "!scss/**/_*.scss", "js/*.js", "js/*.ts"],
       },
     },
-  },
 
-  server: {
-    // required to load scripts from custom host
-    cors: true,
-
-    // we need a strict port to match on PHP side
-    // change freely, but update on PHP to match the same port
-    strictPort: true,
-    port: 12321,
-
-    hmr: {
-      host: "localhost",
+    server: {
+      host: true,
+      port: process.env.VITE_PORT || 12321,
+      strictPort: true,
     },
-  },
-
-  resolve: {
-    alias: {
-      jquery: "/js/jquery.module.js",
-    },
-  },
-};
+  };
+});
